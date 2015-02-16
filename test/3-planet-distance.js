02f6fa37d120893bab92660b5336d1b3a7789c0c
Desmond Morris <hi@desmondmorris.com>
2015-02-16 18:51:57 -0500

    Make test smarter for problem #3

--- test/3-planet-distance.js
+++ test/3-planet-distance.js
@@ -7,7 +7,8 @@ var data = require('./data/planet-distance.json');
 describe('3. Distance between planets', function(){
   it('Calculate distance between two planets', function(){
     for(var property in data) {
-      var distance = problem.apply(this, property.split('-'));
+      var planets = property.split('-');
+      var distance = problem(planets[0].toLowerCase(), planets[1].toLowerCase());
       assert(distance === Math.ceil(data[property] * 92955807.3));
     };
   });
