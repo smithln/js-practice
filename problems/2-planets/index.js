9c93439735ab86ce13307d44aec812c709050514
Lauren Smith <laurenicolesmith@gmail.com>
2015-02-16 18:30:39 -0500

    Solves problem dos :space_invader:

--- problems/2-planets/index.js
+++ problems/2-planets/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var planets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune"
+]
+
+function planetNames() {
+  return(planets);
+}
+
+module.exports = planetNames;
