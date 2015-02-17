463163b86faa987583083d074f7476cd48b8d5d2
Lauren Smith <laurenicolesmith@gmail.com>
2015-02-16 23:46:06 -0500

    Shows problem-3 progress

--- problems/3-planet-distance/index.js
+++ problems/3-planet-distance/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// json file has all planet-distance values
+// each data point needs to convert AU to miles, then round to nearest mile
+// take AU and * 92955807.3
+// math.ceil(planet-planet) after its converted to miles
+
+var mercuryJupiter = 4.81;
+
+
+function planetDistance() {
+  return (Math.ceil(mercuryJupiter * 92955807.3));
+}
+
+module.exports = planetDistance;
