6d1ef187809baae96d21a09e8bb8fca78886b2d3
Lauren Smith <laurenicolesmith@gmail.com>
2015-02-18 10:07:25 -0500

    Progress

--- problems/3-planet-distance/index.js
+++ problems/3-planet-distance/index.js
@@ -1,15 +1,84 @@
 'use strict';
 
-// json file has all planet-distance values
-// each data point needs to convert AU to miles, then round to nearest mile
-// take AU and * 92955807.3
-// math.ceil(planet-planet) after its converted to miles
+// need to identify planet relationship
+// need to give that relationship a distance number
+// need to be able to use that relationship's distance number to x miles conversion
+// need to round that number to nearest integer
 
-var mercuryJupiter = 4.81;
+// var planets = [
+//   'mercury-venus:0.33',
+//   'mercury-earth:0.61'
+// ]
+
+var planets = {
+  'mercury',
+  'venus',
+  'earth',
+  'mars',
+  'jupiter',
+  'saturn',
+  'uranus',
+  'neptune'
+}
+
+var planetRelationships {
+  //string + '-' + string
+}
+
+var distance {
+  //planetRelationships = number
+}
+
+// for 'x' and 'y' identify distance
+// use that distance in function planetDistance z
+// is z a variable? z needs to pull from distance
 
 
 function planetDistance() {
-  return (Math.ceil(mercuryJupiter * 92955807.3));
+  return (Math.ceil(//z * 92955807.3));
 }
 
 module.exports = planetDistance;
+
+// "mercury-venus"= 0.33,
+// "mercury-earth": ,
+// "mercury-mars",
+// "mercury-jupiter",
+// "mercury-saturn",
+// "mercury-uranus",
+// "mercury-neptune",
+// "venus-mercury": 0.33,
+// "venus-earth",
+// "venus-mars",
+// "venus-jupiter",
+// "venus-saturn",
+// "venus-uranus",
+// "venus-neptune",
+// "earth-mercury",
+// "earth-venus",
+// "earth-mars",
+// "earth-saturn",
+// "earth-jupiter",
+// "earth-uranus",
+// "earth-neptune",
+// "mars-mercury",
+// "mars-venus",
+// "mars-earth",
+// "mars-jupiter",
+// "mars-saturn",
+// "mars-uranus",
+// "mars-neptune",
+// "jupiter-mercury",
+// "jupiter-venus",
+// "jupiter-earth",
+// "jupiter-mars",
+// "jupiter-saturn",
+// "jupiter-uranus",
+// "jupiter-neptune",
+// "saturn-mercury",
+// "saturn-venus",
+// "saturn-earth",
+// "saturn-mars",
+// "saturn-jupiter",
+// "saturn-uranus",
+// "saturn-neptune",
